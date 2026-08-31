483289ad5f998f7a281b8c4f7b6610f2565c20c8
RhysLindmark <rhyslindmark@gmail.com>
2026-08-31 15:59:33 -0700

    Add ten global ancient history questions

--- lib/history.test.ts
+++ lib/history.test.ts
@@ -4,7 +4,7 @@ import { dailyHistoryDeck, dailyHistoryQuestionCount, formatHistoryYear, history
 
 void test('history library begins a sourced 1,000-question curriculum', () => {
   assert.equal(historyQuestionTarget, 1000);
-  assert.equal(historyQuestions.length, 33);
+  assert.equal(historyQuestions.length, 43);
   assert.equal(new Set(historyQuestions.map((question) => question.id)).size, historyQuestions.length);
   assert.ok(historyQuestions.every((question) => question.year >= question.min && question.year <= question.max));
   assert.ok(historyQuestions.every((question) => question.sourceUrl.startsWith('https://') && question.verifiedAt === '2026-08-31'));
