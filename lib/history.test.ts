25970c6edee00c77b74dabd69048e6e295a104f7
RhysLindmark <rhyslindmark@gmail.com>
2026-08-31 22:30:25 -0700

    Add global navigation history questions

--- lib/history.test.ts
+++ lib/history.test.ts
@@ -4,7 +4,7 @@ import { dailyHistoryDeck, dailyHistoryQuestionCount, formatHistoryYear, history
 
 void test('history library begins a sourced 1,000-question curriculum', () => {
   assert.equal(historyQuestionTarget, 1000);
-  assert.equal(historyQuestions.length, 73);
+  assert.equal(historyQuestions.length, 83);
   assert.equal(new Set(historyQuestions.map((question) => question.id)).size, historyQuestions.length);
   assert.equal(new Set(historyQuestions.map((question) => question.prompt)).size, historyQuestions.length);
   assert.ok(historyQuestions.every((question) => question.year >= question.min && question.year <= question.max));
@@ -52,6 +52,19 @@ void test('history library begins a sourced 1,000-question curriculum', () => {
     'tenochtitlan-founded',
   ];
   assert.ok(globalPremodernBatch.every((id) => historyQuestions.some((question) => question.id === id)));
+  const navigationAndExchangeBatch = [
+    'lanse-aux-meadows',
+    'maori-settlement-aotearoa',
+    'zheng-he-first-voyage',
+    'dias-cape-good-hope',
+    'vasco-da-gama-calicut',
+    'first-circumnavigation-complete',
+    'manila-galleon-route',
+    'voc-founded',
+    'tasman-pacific-voyage',
+    'cook-first-pacific-voyage',
+  ];
+  assert.ok(navigationAndExchangeBatch.every((id) => historyQuestions.some((question) => question.id === id)));
 });
 
 void test('ancient dates are shown as human BCE/CE labels', () => {
